308f68199edfed55a3527026b5a14e5edaa0c268
Evgeniy Korzunov <korzunov.e.v@gmail.com>
2023-04-20 18:41:30 +0500

    fix(CORS): try define origins

--- src/core/settings/cors.py
+++ src/core/settings/cors.py
@@ -4,7 +4,10 @@ from src.core.settings.base import _BaseModel
 
 
 class CORSSettings(_BaseModel):
-    allow_origins: list[str] = ["172.17.1.8:8080", "172.17.1.19:8080"]
+    allow_origins: list[str] = [
+        "http://172.17.1.8:8080",
+        "http://172.17.1.19:8080",
+    ]
     allow_credentials: bool | None = True
     allow_methods: list[str] = [
         "GET",
